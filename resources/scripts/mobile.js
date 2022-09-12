3f184a4eb233725aac85bc85e9cbdf398691b2f9
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-09-12 13:19:23 -0500

    more mobile formatting, basics finally done

--- resources/scripts/mobile.js
+++ resources/scripts/mobile.js
@@ -1,12 +1,24 @@
 let mobile = document.getElementById('mobile-check');
 
 if ($(mobile).css('display') == 'block') {
-    let fertilizerSelect = document.getElementById('fertilizer');
 
-    let fertilizer = document.createElement('option');
-    fertilizer.setAttribute('value', '');
-    fertilizer.innerHTML = '-- Fertilizer --';
+    let gold = document.getElementById('gold');
+    let level = document.getElementById('farming-level');
+    let days = document.getElementById('days-left');
+    let season = document.getElementById('season');
+    let daysInput = document.getElementById('days');
+    let levelInput = document.getElementById('level');
+    let goldInput = document.getElementById('gold-to-spend');
 
-    $(fertilizerSelect).prepend(fertilizer);
+    gold.innerHTML = 'Gold: ';
+    days.innerHTML = 'Days: ';
+    level.innerHTML = 'Level: ';
+    season.innerHTML = '-- Select Season --';
+    daysInput.style.width = '2rem';
+    daysInput.style.textAlign = 'center';
+    levelInput.style.width = '2rem';
+    levelInput.style.textAlign = 'center';
+    goldInput.style.width = '4rem';
+    goldInput.style.textAlign = 'center';
 
 }
